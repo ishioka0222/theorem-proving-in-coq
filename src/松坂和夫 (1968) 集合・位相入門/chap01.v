ca5edb1181ac9eb4ac3069155fc5b5bcbde419c8
Hiroki Ishioka <ishioka0222@gmail.com>
2021-08-31 20:23:16 +0900

    feat: Formula_2_2_2を追加

--- src/松坂和夫 (1968) 集合・位相入門/chap01.v
+++ src/松坂和夫 (1968) 集合・位相入門/chap01.v
@@ -1,6 +1,7 @@
 From mathcomp
   Require Import ssreflect.
 Require Import Coq.Sets.Ensembles.
+Require Import Coq.Sets.Powerset_facts.
 
 Lemma Formula_1_4 (T : Type) (A B C : Ensemble T) : Included T A B /\ Included T B C -> Included T A C.
 Proof.
@@ -22,4 +23,10 @@ Proof.
   move=> x HxinA.
   left.
   by [].
+Qed.
+
+Lemma Formula_2_2_2 (T : Type) (A B : Ensemble T) : Included T B (Union T A B).
+Proof.
+  rewrite -Union_commutative.
+  apply Formula_2_2_1.
 Qed.
