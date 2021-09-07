15230b5a2feae90e2c25576e55529738ebdf95ab
Hiroki Ishioka <ishioka0222@gmail.com>
2021-09-07 20:17:57 +0900

    feat: Theorem 1.2.(ii)''まで追加

--- src/Michio Suzuki (1982) Group Theory I/main.v
+++ src/Michio Suzuki (1982) Group Theory I/main.v
@@ -63,3 +63,66 @@ Proof.
     rewrite HgeEqe.
     by [].
 Qed.
+
+Definition one (G : Group) : G.
+  destruct (constructive_definite_description (is_one G) (one_exists_unique G)) as [one H].
+  exact one.
+Defined.
+
+Theorem one_is_one (G : Group) : is_one G (one G).
+Proof.
+  remember (one G) as e eqn: H.
+  unfold one in H.
+  destruct (constructive_definite_description (is_one G) (one_exists_unique G)) as [e' He'] in H.
+  rewrite H.
+  exact He'.
+Qed.
+
+Definition are_mutually_inverse (G : Group) (a a' : G) :=
+  mul G a a' = one G /\ mul G a' a = one G.
+
+(* Theorem 1.2.(ii)'' *)
+Theorem inverse_exists_unique (G : Group) (a : G) :
+  exists! a' : G, are_mutually_inverse G a a'.
+Proof.
+  destruct (inv_mul G a (one G)) as [a' Haa'_eq_one].
+  destruct (mul_inv G a (one G)) as [a'' Ha''a_eq_one].
+  
+  assert (a' = a'') as Ha'_eq_a''.
+  + rewrite -(proj1 ((one_is_one G) a'')).
+    rewrite -Haa'_eq_one.
+    rewrite -(mul_assoc G a'' a a').
+    rewrite Ha''a_eq_one.
+    rewrite (proj2 ((one_is_one G) a')).
+    by [].
+  
+  rewrite -Ha'_eq_a'' in Ha''a_eq_one.
+  exists a'.
+  
+  split.
+  + split.
+    + exact Haa'_eq_one.
+    + exact Ha''a_eq_one.
+  + move=> a''' [Hax_eq_one Hxa_eq_one].
+    rewrite -(proj1 ((one_is_one G) a')).
+    rewrite -Hax_eq_one.
+    rewrite -(mul_assoc G a' a a''').
+    rewrite Ha''a_eq_one.
+    rewrite (proj2 ((one_is_one G) a''')).
+    by [].
+Qed.
+
+Definition inv (G : Group) : G -> G.
+  move=> a.
+  destruct (constructive_definite_description (are_mutually_inverse G a) (inverse_exists_unique G a)) as [a' H].
+  exact a'.
+Defined.
+
+Theorem inv_is_inv (G : Group) (a : G) : are_mutually_inverse G a (inv G a).
+Proof.
+  remember (inv G a) as a' eqn: H.
+  unfold inv in H.
+  destruct (constructive_definite_description (are_mutually_inverse G a) (inverse_exists_unique G a)) as [a'' Ha''] in H.
+  rewrite H.
+  exact Ha''.
+Qed.
