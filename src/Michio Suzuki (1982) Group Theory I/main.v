e258f08598738f9c1e444eded9b66c669ec0f6a7
Hiroki Ishioka <ishioka0222@gmail.com>
2021-09-07 20:17:33 +0900

    feat: ensembleからsetに書き換え

--- src/Michio Suzuki (1982) Group Theory I/main.v
+++ src/Michio Suzuki (1982) Group Theory I/main.v
@@ -1,46 +1,45 @@
 (* Suzuki, Michio - Group Theory. I *)
 From mathcomp
   Require Import ssreflect.
-Require Import Coq.Sets.Ensembles.
+Require Import Coq.Logic.Description.
 
 (* Definition 1.1. *)
-Structure Group (T : Type) : Type := mkGroup
+Structure Group : Type := mkGroup
 {
-  G :> Ensemble T;
-  non_emp : exists x : T, In T G x;
-  mul : T -> T -> T;
-  mul_closed : forall a b : T, In T G a -> In T G b -> In T G (mul a b);
-  mul_assoc : forall a b c : T, mul (mul a b) c = mul a (mul b c);
-  inv_mul : forall a b : T, exists x, mul a x = b;
-  mul_inv : forall a b : T, exists y, mul y a = b;
+  G :> Set;
+  inhab : inhabited G;
+  mul : G -> G -> G;
+  mul_assoc : forall a b c : G, mul (mul a b) c = mul a (mul b c);
+  inv_mul : forall a b : G, exists x : G, mul a x = b;
+  mul_inv : forall a b : G, exists y : G, mul y a = b;
 }.
 
 (* Definition 1.3. *)
-Definition is_one (T : Type) (G : Group T) (e : T) :=
-  forall g : T, mul T G g e = g /\ mul T G e g = g.
+Definition is_one (G : Group) (e : G) :=
+  forall g : G, mul G g e = g /\ mul G e g = g.
 
 (* Theorem 1.2.(ii)' *)
-Theorem one_exists_unique (T : Type) (G : Group T) :
-  exists! one : T, is_one T G one.
+Theorem one_exists_unique (G : Group) :
+  exists! one : G, is_one G one.
 Proof.
-  destruct (non_emp T G) as [a HaInG].
-  destruct (inv_mul T G a a) as [e HaeEqa].
-  destruct (mul_inv T G a a) as [e' He'aEqa].
+  destruct (inhab G) as [a].
+  destruct (inv_mul G a a) as [e HaeEqa].
+  destruct (mul_inv G a a) as [e' He'aEqa].
   exists e.
 
-  assert (forall g : T, mul T G g e = g) as He_oner.
+  assert (forall g : G, mul G g e = g) as He_oner.
   + move=> g.
-    destruct (inv_mul T G a g) as [u HauEqg].
-    destruct (mul_inv T G a g) as [v HvaEqg].
+    destruct (inv_mul G a g) as [u HauEqg].
+    destruct (mul_inv G a g) as [v HvaEqg].
     rewrite -HvaEqg.
     rewrite mul_assoc.
     rewrite HaeEqa.
     by [].
 
-  assert (forall g : T, mul T G e' g = g) as He'_onel.
+  assert (forall g : G, mul G e' g = g) as He'_onel.
   + move=> g.
-    destruct (inv_mul T G a g) as [u HauEqg].
-    destruct (mul_inv T G a g) as [v HvaEqg].
+    destruct (inv_mul G a g) as [u HauEqg].
+    destruct (mul_inv G a g) as [v HvaEqg].
     rewrite -HauEqg.
     rewrite -mul_assoc.
     rewrite He'aEqa.
