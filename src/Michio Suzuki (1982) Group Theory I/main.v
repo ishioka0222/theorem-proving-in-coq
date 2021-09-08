9f84dad153d6571baa08c20367194b49faa9d7a6
Hiroki Ishioka <ishioka0222@gmail.com>
2021-09-08 23:16:38 +0900

    refactor: subgroupの定義による名前衝突を回避するためにgroupの定義を変更

--- src/Michio Suzuki (1982) Group Theory I/main.v
+++ src/Michio Suzuki (1982) Group Theory I/main.v
@@ -6,46 +6,46 @@ Require Import Coq.Logic.Description.
 (* Definition 1.1. *)
 Structure group : Type := make_group
 {
-  G :> Set;
-  inhab : inhabited G;
-  mul : G -> G -> G;
-  mul_assoc : forall a b c : G, mul (mul a b) c = mul a (mul b c);
-  eq_l : forall a b : G, exists x : G, mul a x = b;
-  eq_r : forall a b : G, exists y : G, mul y a = b;
+  group_carrier :> Set;
+  group_inhab : inhabited group_carrier;
+  group_mul : group_carrier -> group_carrier -> group_carrier;
+  group_mul_assoc : forall a b c : group_carrier, group_mul (group_mul a b) c = group_mul a (group_mul b c);
+  group_eq_l : forall a b : group_carrier, exists x : group_carrier, group_mul a x = b;
+  group_eq_r : forall a b : group_carrier, exists y : group_carrier, group_mul y a = b;
 }.
 
 (* Definition 1.3. *)
-Definition is_one (G : group) (e : G) :=
-  forall g : G, mul G g e = g /\ mul G e g = g.
+Definition is_group_one (G : group) (e : G) :=
+  forall g : G, group_mul G g e = g /\ group_mul G e g = g.
 
 (* Theorem 1.2.(ii)' *)
-Theorem one_exists_unique (G : group) :
-  exists! one : G, is_one G one.
+Theorem group_one_exists_unique (G : group) :
+  exists! group_one : G, is_group_one G group_one.
 Proof.
-  destruct (inhab G) as [a].
-  destruct (eq_l G a a) as [e HaeEqa].
-  destruct (eq_r G a a) as [e' He'aEqa].
+  destruct (group_inhab G) as [a].
+  destruct (group_eq_l G a a) as [e Hae_eq_a].
+  destruct (group_eq_r G a a) as [e' He'a_eq_a].
   exists e.
 
-  assert (forall g : G, mul G g e = g) as He_oner.
+  assert (forall g : G, group_mul G g e = g) as He_oner.
   + move=> g.
-    destruct (eq_l G a g) as [u HauEqg].
-    destruct (eq_r G a g) as [v HvaEqg].
-    rewrite -HvaEqg.
-    rewrite mul_assoc.
-    rewrite HaeEqa.
+    destruct (group_eq_l G a g) as [u Hau_eq_g].
+    destruct (group_eq_r G a g) as [v Hva_eq_g].
+    rewrite -Hva_eq_g.
+    rewrite group_mul_assoc.
+    rewrite Hae_eq_a.
     by [].
 
-  assert (forall g : G, mul G e' g = g) as He'_onel.
+  assert (forall g : G, group_mul G e' g = g) as He'_onel.
   + move=> g.
-    destruct (eq_l G a g) as [u HauEqg].
-    destruct (eq_r G a g) as [v HvaEqg].
-    rewrite -HauEqg.
-    rewrite -mul_assoc.
-    rewrite He'aEqa.
+    destruct (group_eq_l G a g) as [u Hau_eq_g].
+    destruct (group_eq_r G a g) as [v Hva_eq_g].
+    rewrite -Hau_eq_g.
+    rewrite -group_mul_assoc.
+    rewrite He'a_eq_a.
     by [].
 
-  assert (e = e') as HeEqe'.
+  assert (e = e') as He_eq_e'.
   + rewrite -(He_oner e').
     rewrite (He'_onel e).
     by [].
@@ -54,46 +54,46 @@ Proof.
   + move=> g.
     split.
     + apply (He_oner g).
-    + rewrite HeEqe'.
+    + rewrite He_eq_e'.
       rewrite (He'_onel g).
       by [].
   + move=> g Hg_one.
-    destruct (Hg_one e) as [HegEqe HgeEqe].
+    destruct (Hg_one e) as [Heg_eq_e Hge_eq_e].
     rewrite -(He_oner g).
-    rewrite HgeEqe.
+    rewrite Hge_eq_e.
     by [].
 Qed.
 
-Definition one (G : group) : G.
-  destruct (constructive_definite_description (is_one G) (one_exists_unique G)) as [one H].
-  exact one.
+Definition group_one (G : group) : G.
+  destruct (constructive_definite_description (is_group_one G) (group_one_exists_unique G)) as [group_one H].
+  exact group_one.
 Defined.
 
-Theorem one_is_one (G : group) : is_one G (one G).
+Theorem group_one_is_group_one (G : group) : is_group_one G (group_one G).
 Proof.
-  remember (one G) as e eqn: H.
-  unfold one in H.
-  destruct (constructive_definite_description (is_one G) (one_exists_unique G)) as [e' He'] in H.
+  remember (group_one G) as e eqn: H.
+  unfold group_one in H.
+  destruct (constructive_definite_description (is_group_one G) (group_one_exists_unique G)) as [e' He'] in H.
   rewrite H.
   exact He'.
 Qed.
 
-Definition are_mutually_inverse (G : group) (a a' : G) :=
-  mul G a a' = one G /\ mul G a' a = one G.
+Definition are_mut_inv (G : group) (a a' : G) :=
+  group_mul G a a' = group_one G /\ group_mul G a' a = group_one G.
 
 (* Theorem 1.2.(ii)'' *)
-Theorem inverse_exists_unique (G : group) (a : G) :
-  exists! a' : G, are_mutually_inverse G a a'.
+Theorem group_inv_ex_uni (G : group) (a : G) :
+  exists! a' : G, are_mut_inv G a a'.
 Proof.
-  destruct (eq_l G a (one G)) as [a' Haa'_eq_one].
-  destruct (eq_r G a (one G)) as [a'' Ha''a_eq_one].
+  destruct (group_eq_l G a (group_one G)) as [a' Haa'_eq_one].
+  destruct (group_eq_r G a (group_one G)) as [a'' Ha''a_eq_one].
 
   assert (a' = a'') as Ha'_eq_a''.
-  + rewrite -(proj1 ((one_is_one G) a'')).
+  + rewrite -(proj1 ((group_one_is_group_one G) a'')).
     rewrite -Haa'_eq_one.
-    rewrite -(mul_assoc G a'' a a').
+    rewrite -(group_mul_assoc G a'' a a').
     rewrite Ha''a_eq_one.
-    rewrite (proj2 ((one_is_one G) a')).
+    rewrite (proj2 ((group_one_is_group_one G) a')).
     by [].
 
   rewrite -Ha'_eq_a'' in Ha''a_eq_one.
@@ -104,101 +104,101 @@ Proof.
     + exact Haa'_eq_one.
     + exact Ha''a_eq_one.
   + move=> a''' [Hax_eq_one Hxa_eq_one].
-    rewrite -(proj1 ((one_is_one G) a')).
+    rewrite -(proj1 ((group_one_is_group_one G) a')).
     rewrite -Hax_eq_one.
-    rewrite -(mul_assoc G a' a a''').
+    rewrite -(group_mul_assoc G a' a a''').
     rewrite Ha''a_eq_one.
-    rewrite (proj2 ((one_is_one G) a''')).
+    rewrite (proj2 ((group_one_is_group_one G) a''')).
     by [].
 Qed.
 
-Definition inv (G : group) : G -> G.
+Definition group_inv (G : group) : G -> G.
   move=> a.
-  destruct (constructive_definite_description (are_mutually_inverse G a) (inverse_exists_unique G a)) as [a' H].
+  destruct (constructive_definite_description (are_mut_inv G a) (group_inv_ex_uni G a)) as [a' H].
   exact a'.
 Defined.
 
-Theorem inv_is_inv (G : group) (a : G) : are_mutually_inverse G a (inv G a).
+Theorem group_inv_is_group_inv (G : group) (a : G) : are_mut_inv G a (group_inv G a).
 Proof.
-  remember (inv G a) as a' eqn: H.
-  unfold inv in H.
-  destruct (constructive_definite_description (are_mutually_inverse G a) (inverse_exists_unique G a)) as [a'' Ha''] in H.
+  remember (group_inv G a) as a' eqn: H.
+  unfold group_inv in H.
+  destruct (constructive_definite_description (are_mut_inv G a) (group_inv_ex_uni G a)) as [a'' Ha''] in H.
   rewrite H.
   exact Ha''.
 Qed.
 
 (* Theorem 1.2.(iii).1 *)
-Theorem eq_l_ex_uni (G : group) :
-  forall a b x : G, mul G a x = b -> x = mul G (inv G a) b.
+Theorem group_eq_l_ex_uni (G : group) :
+  forall a b x : G, group_mul G a x = b -> x = group_mul G (group_inv G a) b.
 Proof.
   move=> a b x Hax_eq_b.
   rewrite -Hax_eq_b.
-  rewrite -mul_assoc.
-  rewrite (proj2 (inv_is_inv G a)).
-  rewrite (proj2 (one_is_one G x)).
+  rewrite -group_mul_assoc.
+  rewrite (proj2 (group_inv_is_group_inv G a)).
+  rewrite (proj2 (group_one_is_group_one G x)).
   reflexivity.
 Qed.
 
 (* Theorem 1.2.(iii).2 *)
-Theorem eq_r_ex_uni (G : group) :
-  forall a b y : G, mul G y a = b -> y = mul G b (inv G a).
+Theorem group_eq_r_ex_uni (G : group) :
+  forall a b y : G, group_mul G y a = b -> y = group_mul G b (group_inv G a).
 Proof.
   move=> a b y Hya_eq_b.
   rewrite -Hya_eq_b.
-  rewrite mul_assoc.
-  rewrite (proj1 (inv_is_inv G a)).
-  rewrite (proj1 (one_is_one G y)).
+  rewrite group_mul_assoc.
+  rewrite (proj1 (group_inv_is_group_inv G a)).
+  rewrite (proj1 (group_one_is_group_one G y)).
   reflexivity.
 Qed.
 
 (* Corollary_p4_l7 *)
 Structure group' : Type := make_group'
 {
-  G' :> Set;
-  one' : G';
-  inv' : G' -> G';
-  mul' : G' -> G' -> G';
-  mul_assoc' : forall a b c : G', mul' (mul' a b) c = mul' a (mul' b c);
-  one_mul' : forall a : G', mul' one' a = a;
-  mul_one' : forall a : G', mul' a one' = a;
-  inv_mul' : forall a : G', mul' (inv' a) a = one';
-  mul_inv' : forall a : G', mul' a (inv' a) = one';
+  group'_carrier :> Set;
+  group'_one : group'_carrier;
+  group'_inv : group'_carrier -> group'_carrier;
+  group'_mul : group'_carrier -> group'_carrier -> group'_carrier;
+  group'_mul_assoc : forall a b c : group'_carrier, group'_mul (group'_mul a b) c = group'_mul a (group'_mul b c);
+  group'_one_mul : forall a : group'_carrier, group'_mul group'_one a = a;
+  group'_mul_one : forall a : group'_carrier, group'_mul a group'_one = a;
+  group'_inv_mul : forall a : group'_carrier, group'_mul (group'_inv a) a = group'_one;
+  group'_mul_inv : forall a : group'_carrier, group'_mul a (group'_inv a) = group'_one;
 }.
 
 Definition group_to_group' : group -> group'.
   move=> G.
   exact (make_group'
     G
-    (one G)
-    (inv G)
-    (mul G)
-    (mul_assoc G)
-    (fun a => proj2 (one_is_one G a))
-    (fun a => proj1 (one_is_one G a))
-    (fun a => proj2 (inv_is_inv G a))
-    (fun a => proj1 (inv_is_inv G a))
+    (group_one G)
+    (group_inv G)
+    (group_mul G)
+    (group_mul_assoc G)
+    (fun a => proj2 (group_one_is_group_one G a))
+    (fun a => proj1 (group_one_is_group_one G a))
+    (fun a => proj2 (group_inv_is_group_inv G a))
+    (fun a => proj1 (group_inv_is_group_inv G a))
    ).
 Qed.
 
 Theorem group'_to_group_sub_r (G' : group') :
-  forall a b : G', exists x : G', mul' G' a x = b.
+  forall a b : G', exists x : G', group'_mul G' a x = b.
 Proof.
   move=> a b.
-  exists (mul' G' (inv' G' a) b).
-  rewrite -mul_assoc'.
-  rewrite mul_inv'.
-  rewrite one_mul'.
+  exists (group'_mul G' (group'_inv G' a) b).
+  rewrite -group'_mul_assoc.
+  rewrite group'_mul_inv.
+  rewrite group'_one_mul.
   reflexivity.
 Qed.
 
 Theorem group'_to_group_sub_l (G' : group') :
-  forall a b : G', exists y : G', mul' G' y a = b.
+  forall a b : G', exists y : G', group'_mul G' y a = b.
 Proof.
   move=> a b.
-  exists (mul' G' b (inv' G' a)).
-  rewrite mul_assoc'.
-  rewrite inv_mul'.
-  rewrite mul_one'.
+  exists (group'_mul G' b (group'_inv G' a)).
+  rewrite group'_mul_assoc.
+  rewrite group'_inv_mul.
+  rewrite group'_mul_one.
   reflexivity.
 Qed.
 
@@ -206,9 +206,9 @@ Definition group'_to_group : group' -> group.
   move=> G'.
   exact (make_group
     G'
-    (inhabits (one' G'))
-    (mul' G')
-    (mul_assoc' G')
+    (inhabits (group'_one G'))
+    (group'_mul G')
+    (group'_mul_assoc G')
     (group'_to_group_sub_r G')
     (group'_to_group_sub_l G')
   ).
@@ -216,50 +216,50 @@ Qed.
 
 (* Theorem 1.4.1 *)
 Theorem inv_inv (G : group) :
-  forall a : G, inv G (inv G a) = a.
+  forall a : G, group_inv G (group_inv G a) = a.
 Proof.
   move=> a.
 
-  pose proof (inverse_exists_unique G (inv G a)) as H.
+  pose proof (group_inv_ex_uni G (group_inv G a)) as H.
   rewrite <- unique_existence in H.
   destruct H as [_ Huni].
 
-  pose proof (inv_is_inv G (inv G a)) as Hinvinva.
+  pose proof (group_inv_is_group_inv G (group_inv G a)) as Hinvinva.
 
-  pose proof (inv_is_inv G a) as Ha.
-  unfold are_mutually_inverse in Ha.
+  pose proof (group_inv_is_group_inv G a) as Ha.
+  unfold are_mut_inv in Ha.
   rewrite <- and_comm in Ha.
-  fold (are_mutually_inverse G (inv G a) a) in Ha.
+  fold (are_mut_inv G (group_inv G a) a) in Ha.
 
-  rewrite -(Huni a (inv G (inv G a)) Ha Hinvinva).
+  rewrite -(Huni a (group_inv G (group_inv G a)) Ha Hinvinva).
   reflexivity.
 Qed.
 
 (* Theorem 1.4.2 *)
-Theorem mul_inv_rev (G : group) :
-  forall a b : G, inv G (mul G a b) = mul G (inv G b) (inv G a).
+Theorem group_mul_inv_rev (G : group) :
+  forall a b : G, group_inv G (group_mul G a b) = group_mul G (group_inv G b) (group_inv G a).
 Proof.
   move=> a b.
 
-  pose proof (inverse_exists_unique G (mul G a b)) as H.
+  pose proof (group_inv_ex_uni G (group_mul G a b)) as H.
   rewrite <- unique_existence in H.
   destruct H as [_ Huni].
 
-  pose proof (inv_is_inv G (mul G a b)) as H1.
+  pose proof (group_inv_is_group_inv G (group_mul G a b)) as H1.
 
-  assert (are_mutually_inverse G (mul G a b) (mul G (inv G b) (inv G a))) as H2.
+  assert (are_mut_inv G (group_mul G a b) (group_mul G (group_inv G b) (group_inv G a))) as H2.
   + split.
-    + rewrite (mul_assoc G a b (mul G (inv G b) (inv G a))).
-      rewrite -(mul_assoc G b (inv G b) (inv G a)).
-      rewrite (proj1 (inv_is_inv G b)).
-      rewrite (proj2 (one_is_one G (inv G a))).
-      rewrite (proj1 (inv_is_inv G a)).
+    + rewrite (group_mul_assoc G a b (group_mul G (group_inv G b) (group_inv G a))).
+      rewrite -(group_mul_assoc G b (group_inv G b) (group_inv G a)).
+      rewrite (proj1 (group_inv_is_group_inv G b)).
+      rewrite (proj2 (group_one_is_group_one G (group_inv G a))).
+      rewrite (proj1 (group_inv_is_group_inv G a)).
       reflexivity.
-    + rewrite (mul_assoc G (inv G b) (inv G a) (mul G a b)).
-      rewrite -(mul_assoc G (inv G a) a b).
-      rewrite (proj2 (inv_is_inv G a)).
-      rewrite (proj2 (one_is_one G b)).
-      rewrite (proj2 (inv_is_inv G b)).
+    + rewrite (group_mul_assoc G (group_inv G b) (group_inv G a) (group_mul G a b)).
+      rewrite -(group_mul_assoc G (group_inv G a) a b).
+      rewrite (proj2 (group_inv_is_group_inv G a)).
+      rewrite (proj2 (group_one_is_group_one G b)).
+      rewrite (proj2 (group_inv_is_group_inv G b)).
       reflexivity.
-  exact (Huni (inv G (mul G a b)) (mul G (inv G b) (inv G a)) H1 H2).
+  exact (Huni (group_inv G (group_mul G a b)) (group_mul G (group_inv G b) (group_inv G a)) H1 H2).
 Qed.
